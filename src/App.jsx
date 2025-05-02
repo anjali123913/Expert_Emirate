619d0996a922a2467f8c59d3d3679d1bd05806f9
arvindkumar20 <arvindkumar8960905167@gmail.com>
2025-05-03 00:00:44 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <Navbar/>
       <HeroSection />
       <SecondHero />
-      <PlansSection/>
+      {/* <PlansSection/> */}
     </div>
   );
 }
---
+++ src/components/herosection/ExpertCard.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import image from "../../assets/heroSectionImages/logo.png";
+import bgImage from "../../assets/heroSectionImages/moonBG.png";
+
+const ExpertCard = () => {
+  const font = {
+    fontFamily: "Helvetica Now Display",
+  };
+
+  return (
+    <div className="flex justify-center items-center ">
+      <div
+        className="group relative w-72 h-72 rounded-xl overflow-hidden border-2 border-transparent hover:border-yellow-500 
+        transition-all duration-500 shadow-md hover:shadow-yellow-500"
+        style={{
+          backgroundImage: `url(${bgImage})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        {/* Glow at bottom */}
+        <div className="absolute bottom-0 left-0 w-full h-12 bg-gradient-to-t from-yellow-600/40 to-transparent opacity-0 group-hover:opacity-100 transition-all duration-500 z-0" />
+
+        {/* Centered content */}
+        <div
+          className="absolute z-10 w-full top-[150px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 
+          transition-all duration-1000 group-hover:-translate-y-28 text-center"
+        >
+          <img
+            src={image}
+            alt="Expert Emirates Logo"
+            className="mx-auto mb-2 w-[100px] h-[80px]"
+          />
+          <h2
+            className="text-yellow-400 text-xl font-normal mb-1"
+            style={font}
+          >
+            EXPERT EMIRATES
+          </h2>
+          <p className="text-white text-[10px]" style={font}>
+            TURNING HOPES INTO REALITY
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpertCard;
